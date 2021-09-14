7c6b7ad599998a4e829312095756702ff08d1500
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-09-14 21:22:46 +0300

    adding google api

--- books/books.js
+++ books/books.js
@@ -2,9 +2,12 @@
 const express = require('express');
 const server = express();
 require('dotenv').config();
+server.use(express.json());
 const cors = require('cors');
 const mongoose = require('mongoose');
+const { default: axios } = require('axios');
 server.use(cors());
+const googleKey = process.env.Google_key
 
 
 
@@ -29,28 +32,28 @@ async function main() {
  books = mongoose.model('Books', booksSchema);
 // saving()
 }
-
+// used to add books 
 async function saving() {
-    const ramiBbooks = new books({
-        title: 'The Subtle Art of Not Giving a F*ck',
-        img:'https://productimages.hepsiburada.net/s/27/375/10193206050866.jpg',
-        description:'For decades, we’ve been told that positive thinking is the key to a happy, rich life. "F**k positivity," Mark Manson says. "Let’s be honest, shit is f**ked and we have to live with it." In his wildly popular Internet blog, Manson doesn’t sugarcoat or equivocate. He tells it like it is—a dose of raw, refreshing, honest truth that is sorely lacking today. The Subtle Art of Not Giving a F**k is his antidote to the coddling, let’s-all-feel-good mindset that has infected modern society and spoiled a generation, rewarding them with gold medals just for showing up.',
-        status:'Avalible',
-        email:'rami-zaitoun@hotmail.com',
-        author: 'Mark Manson',
-        pages: 224,
+    // const ramiBbooks = new books({
+    //     title: 'The Subtle Art of Not Giving a F*ck',
+    //     img:'https://productimages.hepsiburada.net/s/27/375/10193206050866.jpg',
+    //     description:'For decades, we’ve been told that positive thinking is the key to a happy, rich life. "F**k positivity," Mark Manson says. "Let’s be honest, shit is f**ked and we have to live with it." In his wildly popular Internet blog, Manson doesn’t sugarcoat or equivocate. He tells it like it is—a dose of raw, refreshing, honest truth that is sorely lacking today. The Subtle Art of Not Giving a F**k is his antidote to the coddling, let’s-all-feel-good mindset that has infected modern society and spoiled a generation, rewarding them with gold medals just for showing up.',
+    //     status:'Avalible',
+    //     email:'rami-zaitoun@hotmail.com',
+    //     author: 'Mark Manson',
+    //     pages: 224,
    
-       });
-       const user = new books({
-        title: 'In Search of Lost Time',
-        img:'https://m.media-amazon.com/images/I/411fuVxxG4L.jpg',
-        description:'Swanns Way, the first part of A la recherche de temps perdu, Marcel Prousts seven-part cycle, was published in 1913. In it, Proust introduces the themes that run through the entire work. The narrator recalls his childhood, aided by the famous madeleine; and describes M. Swanns passion for Odette. The work is incomparable. Edmund Wilson said "[Proust] has supplied for the first time in literature an equivalent in the full scale for the new theory of modern physics."',
-        status:'Avalible',
-        email:'rami-zaitoun@hotmail.com',
-        author: 'Marcel Proust',
-        pages: 4215,
+    //    });
+    //    const user = new books({
+    //     title: 'In Search of Lost Time',
+    //     img:'https://m.media-amazon.com/images/I/411fuVxxG4L.jpg',
+    //     description:'Swanns Way, the first part of A la recherche de temps perdu, Marcel Prousts seven-part cycle, was published in 1913. In it, Proust introduces the themes that run through the entire work. The narrator recalls his childhood, aided by the famous madeleine; and describes M. Swanns passion for Odette. The work is incomparable. Edmund Wilson said "[Proust] has supplied for the first time in literature an equivalent in the full scale for the new theory of modern physics."',
+    //     status:'Avalible',
+    //     email:'rami-zaitoun@hotmail.com',
+    //     author: 'Marcel Proust',
+    //     pages: 4215,
    
-       });
+    //    });
        const ahmad = new books({
         title: 'Crime and Punishment',
         img:'https://images-na.ssl-images-amazon.com/images/I/81b6e-dc41L.jpg',
@@ -61,8 +64,8 @@ async function saving() {
         pages: 700,
    
        });
-    await ramiBbooks.save();
-    await user.save();
+    // await ramiBbooks.save();
+    // await user.save();
     await ahmad.save();
  }
 
@@ -80,4 +83,49 @@ function booksHandler(req,res)
     })
 }
 
+// add book
+
+async function addHandler(req,res) { 
+    const googleName = req.body.BookName
+
+   
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${googleName}&key=${googleKey}&maxResults=1`)
+    .then((results) => {
+      const bookArray = results.data.items
+   
+       let googleArray= bookArray.map((book) => {
+              
+                return new GoogleBooks(book)
+        })
+        // res.send(googleArray)
+        googleArray.map((item) => {
+            books.create({ 
+                title: item.title,
+                img: item.image,
+                description: item.desc,
+                status: item.status,
+                email: 'rami-zaitoun@hotmail.com',
+                author: item.auth[0],
+                pages: item.pages,
+            });
+          })
+   
+    })
+   
+ }
+//using google api 
+function GoogleBooks(book){
+    this.title = book.volumeInfo.title;
+    this.image= book.volumeInfo.imageLinks.thumbnail;
+    this.desc= book.volumeInfo.description;
+    this.auth= book.volumeInfo.authors;
+    this.status= book.volumeInfo.printType;
+    this.pages=book.volumeInfo.pageCount;
+}
+
+ // books Api 
+
+
+
 module.exports = booksHandler
+module.exports = addHandler
--- server.js
+++ server.js
@@ -3,18 +3,18 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const booksHandler = require('./books/books')
+const booksHandler = require('./books/books');
+const addHandler = require('./books/books')
+
 
 const server = express();
+server.use(express.json());
 server.use(cors());
 
 const PORT = process.env.PORT;
 // Routes
 server.get('/books', booksHandler)
-server.get('/test', (request, response) => {
+server.post('/addbook', addHandler)
 
-  response.send('test request received')
-
-})
 
 server.listen(PORT, () => console.log(`listening on ${PORT}`));
